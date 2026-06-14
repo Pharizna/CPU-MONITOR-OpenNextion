c417a26a8065bde82a46a97f317ae74c4799f2d9
Pharizna <pharizna@gmail.com>
2026-06-14 12:59:04 +0200

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// ===============================
+// WIFI
+// ===============================
+#define WIFI_SSID   "Pharizna"
+#define WIFI_PASS   "Pharizna2010"
+
+// ===============================
+// MQTT
+// ===============================
+#define MQTT_SERVER "192.168.1.144"
+#define MQTT_PORT   1883
+#define MQTT_USER   "pharizna"
+#define MQTT_PASS   "Pharizna2010"
